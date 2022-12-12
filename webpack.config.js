b6ffaffc78e4a225a6a7418b46c308e939a79ee1
anroso11 <andresroberto1187@gmail.com>
2022-12-12 18:25:22 -0500

    Actualizacion repositorio para Gitpages

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/'
+        filename: 'React-Shop/bundle.js',
+        publicPath: '/React-Shop/'
         
     },
     mode: 'development',
